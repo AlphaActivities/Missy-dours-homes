974f22398aa34e3f1c0143c1b353df0a84af381a
Josh the Man <gta.alpha.activities@gmail.com>
2026-02-21 20:32:55 +0000

    Updated index.css

---
+++ src/components/ui/ShimmerButton.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from 'react';
+
+interface ShimmerButtonProps {
+  children: ReactNode;
+  className?: string;
+  onClick?: () => void;
+}
+
+export function ShimmerButton({ children, className = '', onClick }: ShimmerButtonProps) {
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      className={`relative ${className}`}
+    >
+      {children}
+    </button>
+  );
+}
