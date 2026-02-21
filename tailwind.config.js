cb23179ec6b4755a55cc319bd55060128edb91da
Josh the Man <gta.alpha.activities@gmail.com>
2026-02-21 20:32:07 +0000

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shimmer: 'shimmer 4s ease-in-out infinite',
+      },
+    },
   },
   plugins: [],
 };
